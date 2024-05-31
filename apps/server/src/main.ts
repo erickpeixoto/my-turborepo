b349d03861f7df40612ea09b5d3a176b39fc415c
erickpeixoto <erickepeixoto@gmail.com>
2024-05-31 18:21:37 -0300

    ci(vercel): adjusting build

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen('0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
